1ce6fd7a11b931fc3e171f221a761dcce493da3a
wafiben <wafibenjeddou@gmail.com>
2022-02-26 17:35:32 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "./Redux/Action";
+import { useState } from "react";
+import ListOFUsers from "./ListOFUsers";
 function App() {
+  const dispatch = useDispatch();
+  const [user, setUser] = useState({ email: "", password: "" });
+  const handleChange = (event) => {
+    setUser({ ...user, [event.target.id]: event.target.value });
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    dispatch(addUser(user));
+    setUser({ email: "", password: "" })
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={onSubmit}>
+        <div>
+          <p>email</p>
+          <input type="email" id="email" value={user.email} onChange={handleChange} />
+        </div>
+        <div>
+          <p>password</p>
+          <input type="password" id="password" value={user.password} onChange={handleChange} />
+        </div>
+
+        <button type="submit">submit</button>
+      </form>
+      <ListOFUsers />
     </div>
   );
 }
